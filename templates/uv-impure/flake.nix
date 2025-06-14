813729058bb94aa461ab5560470e78d794e846a7
cocvu <cocvu@nightcord.unknown.tld>
2025-06-14 03:04:05 +0000

    uv-impure

--- templates/uv-impure/flake.nix
+++ templates/uv-impure/flake.nix
@@ -8,26 +8,8 @@
   };
 
   outputs =
-    inputs@{ self, flake-parts, ... }:
+    inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      flake = {
-        templates.default = self.templates.uv-impure;
-        templates = {
-          uv-impure = {
-            description = "A flake that creates a uv venv";
-            path = ./templates/uv-impure;
-            welcomeText = ''
-              # uv impure
-              ## Intended Usage
-              Use `uv` command to initialize and manipulate the project.
-
-              The venv is automatically created and activated when entering 
-              the dev shell.  It is recommended to use direnv to enter the dev
-              shell, the corresponding `.envrc` is also provided.
-            '';
-          };
-        };
-      };
       imports = [
         inputs.pre-commit-hooks.flakeModule
       ];
@@ -51,13 +33,25 @@
             src = ./.;
             hooks = {
               nixfmt-rfc-style.enable = true;
+              ruff.enable = true;
+              ruff-format.enable = true;
             };
           };
           devShells = {
             default = pkgs.mkShellNoCC {
+              buildInputs = with pkgs; [
+                python313
+                uv
+                pythonManylinuxPackages.manylinux2014Package
+              ];
               shellHook = ''
                 # install pre-commit hooks
                 ${config.pre-commit.installationScript}
+
+                export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:${pkgs.pythonManylinuxPackages.manylinux2014Package}/lib:$LD_LIBRARY_PATH";
+                uv venv --allow-existing
+                . .venv/bin/activate
+                uv sync
               '';
             };
           };
